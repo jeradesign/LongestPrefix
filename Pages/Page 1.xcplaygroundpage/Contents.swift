b44b42c185d3719026a3ff9f6f02ccb2555e9f00
John Brewer <jbrewer@jera.com>
2024-11-16 10:39:15 -0800

    Josh Homann's solution

--- Pages/Page 1.xcplaygroundpage/Contents.swift
+++ Pages/Page 1.xcplaygroundpage/Contents.swift
@@ -1,31 +1,18 @@
-import Cocoa
+import PlaygroundSupport
 
 class Solution {
     func longestCommonPrefix(_ strs: [String]) -> String {
-        guard strs.count > 0 else {
-            return ""
+        guard let firstString = strs.first else { return "" }
+        var firstIterator = strs.first?.makeIterator()
+        var iterators = strs.dropFirst().map { $0.makeIterator() }
+        var count = 0
+        while true {
+            guard let nextCharacter = firstIterator?.next() else { break }
+            guard iterators.indices.allSatisfy({ iterators[$0].next() == nextCharacter }) else { break }
+            count += 1
         }
-        guard strs.count > 1 else {
-            return strs[0]
-        }
-        
-        let charArrays = strs.map { [Character]($0) }
-
-//        let minlen = charArrays.map { $0.count } .min() ?? 0
-        let minlen = charArrays.lazy.map(\.count) .min() ?? 0
-//        print("minlen = \(minlen)")
-        var result = [Character]()
-        for i in 0..<minlen {
-            let firstCharArray = charArrays[0]
-            let firstChar = firstCharArray[i]
-            for charArray in charArrays[1..<charArrays.count] {
-                if charArray[i] != firstChar {
-                    return String(result)
-                }
-            }
-            result.append(firstChar)
-        }
-        return String(result)
+        let endIndex = firstString.index(firstString.startIndex, offsetBy: count)
+        return String(firstString[..<endIndex])
     }
 }
 
@@ -34,4 +21,3 @@ let solution = Solution()
 print(solution.longestCommonPrefix([String]()))
 print(solution.longestCommonPrefix(["Sphynx"]))
 print(solution.longestCommonPrefix(["Sphynx", "Sphblack", "Sphquartz", "Sphjudge"]))
-//: [Next](@next)
